26e29e344e97dcbd9ba75436afb96eddcb62de21
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-06-08 21:25:19 +0530

    AuthApplication class corrected

--- auth/src/main/java/com/sobanscode/auth/AuthApplication.java
+++ auth/src/main/java/com/sobanscode/auth/AuthApplication.java
@@ -1,9 +1,12 @@
-package com.sobanscode.auth.data;
+package com.sobanscode.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+//@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class AuthApplication {
 
     public static void main(String[] args) {
