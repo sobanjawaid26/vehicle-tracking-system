6efac240c8f9ec94f3f6c931e8d5fcef15cecbff
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-06-09 10:54:29 +0530

    COMPANY MICROSERVICE repository corrected added

--- company/src/main/java/com/sobanscode/company/data/repository/ICompanyRepository.java
+++ company/src/main/java/com/sobanscode/company/data/repository/ICompanyRepository.java
@@ -1,7 +1,8 @@
 package com.sobanscode.company.data.repository;
 
 import com.sobanscode.company.data.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ICompanyRepository {
+public interface ICompanyRepository extends JpaRepository<Company, Long> {
     Company findCompanyByName(String companyName);
 }
