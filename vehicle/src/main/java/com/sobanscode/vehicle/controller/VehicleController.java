ac1371e6ef27645710e09e5d7d17977a21c24c91
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-06-09 09:40:29 +0530

    Save Vehicle API added in VehicleController.java

--- vehicle/src/main/java/com/sobanscode/vehicle/controller/VehicleController.java
+++ vehicle/src/main/java/com/sobanscode/vehicle/controller/VehicleController.java
@@ -1,10 +1,15 @@
 package com.sobanscode.vehicle.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sobanscode.vehicle.dto.request.VehicleSaveDTO;
+import com.sobanscode.vehicle.dto.response.VehicleResponseDTO;
 import com.sobanscode.vehicle.service.VehicleService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import static com.sobanscode.vehicle.constants.ApiUrl.*;
+import static com.sobanscode.vehicle.constants.SwaggerDescriptions.DESCRIPTION1;
 
 @RestController
 @RequestMapping(VEHICLES)
@@ -14,5 +19,12 @@ public class VehicleController {
 
     public VehicleController(VehicleService vehicleService, ObjectMapper objectMapper) {
         this.vehicleService = vehicleService;
+    }
+
+    @PostMapping(SAVE)
+    @Operation(description = DESCRIPTION1)
+    public ResponseEntity<VehicleResponseDTO> save(@RequestHeader("X-User") String generalRequestHeader, @RequestBody VehicleSaveDTO vehicleSaveDTO) {
+        VehicleResponseDTO savedVehicle = vehicleService.saveVehicle(generalRequestHeader, vehicleSaveDTO);
+        return ResponseEntity.ok(savedVehicle);
     }
 }
