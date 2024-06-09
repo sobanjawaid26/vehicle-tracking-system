58ebc026a96b390410f0f061976ef4c69cf6404f
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-06-09 09:07:52 +0530

    VEHICLE SERVICE feign added

---
+++ vehicle/src/main/java/com/sobanscode/vehicle/feign/IGroupServiceFeign.java
@@ -0,0 +1,29 @@
+package com.sobanscode.vehicle.feign;
+
+import com.sobanscode.vehicle.dto.GroupDTO;
+import com.sobanscode.vehicle.dto.response.VehicleResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Set;
+
+@FeignClient(name = "group-service", url = "http://localhost:8082/api/v1/groups")
+public interface IGroupServiceFeign {
+    @GetMapping("/search-by-name")
+    GroupDTO getGroupByName(@RequestHeader("X-User") String generalRequestHeader, @RequestParam("groupName") String groupName);
+
+    @GetMapping("/user-groups-list")
+    Set<GroupDTO> getGroupListByUser(@RequestHeader("X-User") String generalRequestHeader);
+
+    @GetMapping("/user-vehicles-list")
+    Set<VehicleResponseDTO> getVehiclesListByUser(@RequestHeader("X-User") String generalRequestHeader);
+
+    @GetMapping("/user-vehicles-list-by-user-id")
+    Set<VehicleResponseDTO> getVehiclesListByUserId(@RequestHeader("X-User") String generalRequestHeader, @RequestParam("userId") Long userId);
+
+    @GetMapping("/{id}")
+    GroupDTO getById(@RequestHeader("X-User") String generalRequestHeader, @PathVariable("id") Long id);
+}
---
+++ vehicle/src/main/java/com/sobanscode/vehicle/feign/IUserServiceFeign.java
@@ -0,0 +1,13 @@
+package com.sobanscode.vehicle.feign;
+
+import com.sobanscode.vehicle.dto.response.UserResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(url = "http://localhost:8083/api/v1/users",name = "user-service")
+public interface IUserServiceFeign {
+    @GetMapping("{id}")
+    ResponseEntity<UserResponseDTO> getUserById(@PathVariable("id") Long id);
+}
