cc82c9d5ecdb04b60c6c11f7ea99e12e80d02ca6
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-06-09 20:15:46 +0530

    GROUP MICROSERVICE GroupServiceHelper methods added

--- group/src/main/java/com/sobanscode/group/data/dal/GroupServiceHelper.java
+++ group/src/main/java/com/sobanscode/group/data/dal/GroupServiceHelper.java
@@ -1,7 +1,16 @@
 package com.sobanscode.group.data.dal;
 
+import com.sobanscode.group.data.entity.Group;
+import com.sobanscode.group.data.entity.GroupToGroup;
+import com.sobanscode.group.data.entity.UserGroupAuthorization;
 import com.sobanscode.group.data.repository.*;
+import com.sobanscode.group.dto.request.GroupSaveDTO;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.Set;
 
 @Component
 public class GroupServiceHelper {
@@ -16,16 +25,54 @@ public class GroupServiceHelper {
         this.userGroupAuthRepository = userGroupAuthRepository;
     }
 
-    //TODO saveGroup
-    //TODO saveUserGroupAuth
-    //TODO getGroupByIdAndCompanyId
-    //TODO saveGroupToGroup
-    //TODO getChildren
-    //TODO getUserGroupAuth
-    //TODO findRootGroupsByCompanyId
-    //TODO getParent
-    //TODO deleteGroupToGroup
-    //TODO deleteGroupToGroupByParentIdOrChildId
-    //TODO deleteGroup
-    //TODO deleteUserGroupAuthByGroupId
+    public Group saveGroup(Group group){
+        return groupRepository.save(group);
+    }
+
+    public void saveUserGroupAuth(UserGroupAuthorization userGroupAuthorization){
+        userGroupAuthRepository.save(userGroupAuthorization);
+    }
+
+    public Optional<Group> getGroupByIdAndCompanyId(Long groupId, Long companyId){
+        return groupRepository.findByIdAndCompanyId(groupId, companyId);
+    }
+
+    public void saveGroupToGroup(Group parentGroup, Group childGroup){
+        GroupToGroup groupToGroup = GroupToGroup.builder()
+                .parentGroup(parentGroup)
+                .childGroup(childGroup)
+                .build();
+        groupToGroupRepository.save(groupToGroup);
+    }
+
+    public Set<Group> getChildren(Group group){
+        return groupToGroupRepository.findAllByParentGroup(group.getId());
+    }
+
+    public Set<Group> getUserGroupAuth(long userId){
+        return userGroupAuthRepository.findAllGroupsByUser(userId);
+    }
+
+    public Set<Group> findRootGroupsByCompanyId(Long companyId){
+        return groupRepository.findAllByCompanyIdAndAndRootIsTrue(companyId);
+    }
+
+    public Optional<Group> getParent(Group child){
+        return groupToGroupRepository.findByChildGroup(child).map(GroupToGroup::getParentGroup);
+    }
+
+    public Integer deleteGroupToGroup(Group parent, Group child){
+        return groupToGroupRepository.deleteByParentGroupAndChildGroup(parent, child);
+    }
+    public void deleteGroupToGroupByParentIdOrChildId(Group parent, Group group){
+        groupToGroupRepository.deleteByParentGroupIdOrChildGroupId(parent.getId(), group.getId());
+    }
+
+    public void deleteGroup(Group group){
+        groupRepository.deleteByIdAndCompanyId(group.getId(), group.getCompany().getId());
+    }
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void deleteUserGroupAuthByGroupId(long groupId){
+        userGroupAuthRepository.deleteByGroupId(groupId);
+    }
 }
