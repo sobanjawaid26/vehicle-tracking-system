eb5bac361a7e5e073f753de88aa7ea6b087e4b2a
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-06-09 10:07:43 +0530

    GROUP MICROSERVICE started

---
+++ group/src/main/java/com/sobanscode/group/GroupApplication.java
@@ -0,0 +1,12 @@
+package com.sobanscode.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupApplication.class);
+    }
+}
