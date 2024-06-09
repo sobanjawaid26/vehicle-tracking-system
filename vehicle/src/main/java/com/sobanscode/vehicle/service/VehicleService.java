075d85acaff528623a8e795c5707a241fa610391
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-06-09 09:30:33 +0530

    SaveVehicle and generalReqHeadConv added in VehicleService class of VEHICLE MICROSERVICE

--- vehicle/src/main/java/com/sobanscode/vehicle/service/VehicleService.java
+++ vehicle/src/main/java/com/sobanscode/vehicle/service/VehicleService.java
@@ -1,13 +1,25 @@
 package com.sobanscode.vehicle.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sobanscode.vehicle.data.dal.VehicleServiceHelper;
+import com.sobanscode.vehicle.data.entity.Company;
+import com.sobanscode.vehicle.data.entity.Vehicle;
+import com.sobanscode.vehicle.data.enums.Roles;
+import com.sobanscode.vehicle.dto.GroupDTO;
+import com.sobanscode.vehicle.dto.request.GeneralRequestHeaderDTO;
+import com.sobanscode.vehicle.dto.request.VehicleSaveDTO;
+import com.sobanscode.vehicle.dto.response.VehicleResponseDTO;
+import com.sobanscode.vehicle.exception.ApiError;
+import com.sobanscode.vehicle.exception.VehicleServiceException;
 import com.sobanscode.vehicle.feign.IGroupServiceFeign;
 import com.sobanscode.vehicle.feign.IUserServiceFeign;
 import com.sobanscode.vehicle.mapper.IGroupMapper;
 import com.sobanscode.vehicle.mapper.IUserMapper;
 import com.sobanscode.vehicle.mapper.IVehicleMapper;
 import org.springframework.stereotype.Service;
+
+import static com.sobanscode.vehicle.exception.ApiError.USER_NOT_COMPANY_ADMIN;
 
 @Service
 public class VehicleService {
@@ -33,6 +45,24 @@ public class VehicleService {
 
 
     // TODO saveVehicle
+    public VehicleResponseDTO saveVehicle(String generalRequestHeader, VehicleSaveDTO vehicleSaveDTO){
+
+        GeneralRequestHeaderDTO generalRequestHeaderDTO =
+                generalRequestHeaderConverter(generalRequestHeader);
+
+        if(generalRequestHeaderDTO.getRole() != Roles.COMPANY_ADMIN)
+            throw new VehicleServiceException(USER_NOT_COMPANY_ADMIN);
+
+        GroupDTO groupDTO = groupServiceFeign.getById(generalRequestHeader, vehicleSaveDTO.getGroupId());
+
+        Vehicle vehicle = vehicleMapper.toEntity(vehicleSaveDTO);
+        vehicle.setGroup((groupMapper.toEntity(groupDTO)));
+        Company company = new Company();
+        company.setId(generalRequestHeaderDTO.getCompanyId());
+        vehicle.setCompany(company);
+
+        return vehicleMapper.toDto(vehicleServiceHelper.saveVehicle(vehicle));
+    }
     // TODO saveAll
     // TODO getAllVehicles
     // TODO getVehicleById
@@ -42,6 +72,15 @@ public class VehicleService {
     // TODO deleteVehicleByIdAndCompanyId
     // TODO isAdmin
     // TODO generalRequestHeaderConverter
+    GeneralRequestHeaderDTO generalRequestHeaderConverter(String generalRequestHeader){
+        try {
+            GeneralRequestHeaderDTO generalRequestHeaderDTO =
+                    objectMapper.readValue(generalRequestHeader, GeneralRequestHeaderDTO.class);
+            return generalRequestHeaderDTO;
+        } catch (JsonProcessingException e){
+            throw new RuntimeException(e);
+        }
+    }
     // TODO authorizeUserToVehicle
     // TODO getAllVehiclesByGroupId
     // TODO isUserAuthorizedToGroup
