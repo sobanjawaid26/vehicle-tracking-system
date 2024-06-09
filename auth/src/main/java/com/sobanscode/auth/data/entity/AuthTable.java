78c281e13af402807c64ed44653ad7fca15596f9
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-06-09 08:05:38 +0530

    Commenting @Size annotation to fix exception

--- auth/src/main/java/com/sobanscode/auth/data/entity/AuthTable.java
+++ auth/src/main/java/com/sobanscode/auth/data/entity/AuthTable.java
@@ -25,17 +25,17 @@ public class AuthTable {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Size(max = 100)
+    //@Size(max = 100)
     @NotNull
     @Column(name = "username", nullable = false, length = 100)
     private String username;
 
-    @Size(max = 100)
+    //@Size(max = 100)
     @NotNull
     @Column(name = "password", nullable = false, length = 100)
     private String password;
 
-    @Size(max = 20)
+    //@Size(max = 20)
     @Column(name = "role", length = 20)
     @Enumerated(EnumType.STRING)
     private Roles role;
